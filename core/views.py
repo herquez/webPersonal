760b0b4add01198d13506dc6c81b8f3d50b19e47
herquez <miguelherquez@gmail.com>
2023-05-06 19:49:38 -0600

    Agrega al core_views el renderizado de archivos *.html desde templates/core/

--- core/views.py
+++ core/views.py
@@ -1,24 +1,13 @@
-from django.shortcuts import render, HttpResponse
-
-template = """
-<h1>Web Personal | Miguel Hernández</h1>
-
-<ul>
-    <li><a href='/'>Pagina de inicio</a></li>
-    <li><a href='/about/'>Acerca de mi</a></li>
-    <li><a href='/portfolio/'>Portafolio</a></li>
-    <li><a href='/contact/'>Contacto</a></li>
-</ul>
-"""
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse(template + '<h2>Pagina de inicio</h2>')
+    return render(request, 'core/home.html')
 
 def about(request):
-    return HttpResponse(template + '<h2>Acerca de mi</h2>')
+    return render(request, 'core/about.html')
 
 def portfolio(request):
-    return HttpResponse(template + '<h2>Portafolio</h2>')
+    return render(request, 'core/portfolio.html')
 
 def contact(request):
-    return HttpResponse(template + '<h2>Contacto</h2>')
+    return render(request, 'core/contact.html')
