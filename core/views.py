e85939ef96f8626a3ba67ae442eba036f53c068c
herquez <miguelherquez@gmail.com>
2023-05-06 18:56:00 -0600

    Pagina con menú de navegación y paginas de inicio, acerca de mi, portafolio y contacto

--- core/views.py
+++ core/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, HttpResponse
 
+template = """
+<h1>Web Personal | Miguel Hernández</h1>
+
+<ul>
+    <li><a href='/'>Pagina de inicio</a></li>
+    <li><a href='/about/'>Acerca de mi</a></li>
+    <li><a href='/portfolio/'>Portafolio</a></li>
+    <li><a href='/contact/'>Contacto</a></li>
+</ul>
+"""
+
 def home(request):
-    return HttpResponse("<h1>Home page</h1>")
+    return HttpResponse(template + '<h2>Pagina de inicio</h2>')
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return HttpResponse(template + '<h2>Acerca de mi</h2>')
+
+def portfolio(request):
+    return HttpResponse(template + '<h2>Portafolio</h2>')
+
+def contact(request):
+    return HttpResponse(template + '<h2>Contacto</h2>')
