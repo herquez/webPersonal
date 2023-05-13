81a02836bf6a9121f286ae70bc5b196bf3811d29
herquez <miguelherquez@gmail.com>
2023-05-13 13:57:11 -0600

    Agrega las configuraciones de la clase portfolio. Crea URL y ruta de archivos multimedia

--- webPersonal/settings.py
+++ webPersonal/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portfolio',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +111,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
