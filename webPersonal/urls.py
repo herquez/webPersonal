2accd28cf50d5e99405f54e6da04467be61e2b16
herquez <miguelherquez@gmail.com>
2023-05-13 13:58:02 -0600

    Agrega url de archivos multimedia en caso de estar en modo DEBUG

--- webPersonal/urls.py
+++ webPersonal/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path('contact/', core_views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
