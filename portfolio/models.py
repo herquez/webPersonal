2dfc2411953bbbfc8b413c1055c08978ac35257d
herquez <miguelherquez@gmail.com>
2023-05-13 13:56:24 -0600

    Agrega etiquetas personalizadas a cada atributo. Agrega carpeta para archivos multimedia del modelo. Oculta el campo eliminado en el panel de cotnrol. Agrega etiqueta a la tabla del modelo en el panel de control. Ordena los registros en panel de control. Crea la funcion __str__ para retornar nombre de la clase.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    deleted = models.DateTimeField(default=None, null=True)
+    title = models.CharField(max_length=200, verbose_name='Título')
+    description = models.TextField(verbose_name='Descripción')
+    image = models.ImageField(verbose_name='Imagen', upload_to='projects')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Actualizado')
+    deleted = models.DateTimeField(blank=True, null=True, editable=False, verbose_name='Eliminado')
+
+    class Meta:
+        verbose_name = 'Proyecto'
+        verbose_name_plural = 'Proyectos'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
